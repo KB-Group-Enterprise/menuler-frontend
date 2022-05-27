cb222160b83f1bdb43f0ad1b8590215291016e4b
Kaebi <klitbtri666@gmail.com>
2022-05-27 15:23:57 +0700

    [Update] route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/HomePage.vue';
 import { auth } from '../providers/auth';
 import TestingPage from '../pages/TestingPage.vue';
-import CustomerSelectMenuPage from '../pages/CustomerSelectMenuPage.vue';
-import BusinessEditInformationPage from '../pages/BusinessEditInformationPage.vue';
-import BusinessEditMenuPage from '../pages/BusinessEditMenuPage.vue';
-import BusinessEditTableQRPage from '../pages/BusinessEditTableQRPage.vue';
-import BusinessLoginPage from '../pages/BusinessLoginPage.vue';
+import CustomerSelectMenuPage from '../pages/customer/CustomerSelectMenuPage.vue';
+import BusinessEditInformationPage from '../pages/business/BusinessEditInformationPage.vue';
+import BusinessEditMenuPage from '../pages/business/BusinessEditMenuPage.vue';
+import BusinessEditTableQRPage from '../pages/business/BusinessEditTableQRPage.vue';
+import BusinessLoginPage from '../pages/business/BusinessLoginPage.vue';
 
 
 
@@ -21,7 +21,7 @@ const routes = [
   // { name: 'Purchase', path: '/purchase', component: Purchase },
   // { name: 'Status', path: '/admin/status', component: AdminStatusPage },
   { name: 'Test', path: '/test', component: TestingPage },
-  { name: 'CustomerSelectMenuPage', path: '/customer/menu', component: CustomerSelectMenuPage },
+  { name: 'CustomerSelectMenuPage', path: '/customer/menu/:token', component: CustomerSelectMenuPage },
   { name: 'BusinessEditInformationPage', path: '/business', component: BusinessEditInformationPage },
   { name: 'BusinessEditMenuPage', path: '/business/menu', component: BusinessEditMenuPage },
   { name: 'BusinessEditTableQRPage', path: '/business/table', component: BusinessEditTableQRPage },
