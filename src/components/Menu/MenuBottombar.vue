bb8ecc139c128bb3e9c6efe9a1fb8919569e7585
Kaebi <klitbtri666@gmail.com>
2022-05-27 15:22:07 +0700

    [Add] MenuBottonbar

---
+++ src/components/Menu/MenuBottombar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="w-full bg-white h-14 flex md:px-10 px-2 items-center shadow text-main justify-between"
+  >
+  <div class="w-full flex justify-center items-center">
+    ตระกร้าสินค้า x ชิ้น
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGlobal, closeSideBar } from '@/providers/global';
+import BaseToggleMenu from '../Base/BaseToggleMenu.vue';
+import { useAuth } from '@/providers/auth';
+import { modalMenuSelect,modalMenuBasket } from '@/composable/menu-state';
+
+console.log(!(modalMenuSelect && modalMenuBasket));
+
+
+
+</script>
+
+<style scoped></style>
