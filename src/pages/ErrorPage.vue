0bb679d2f77cdc578f0323396f0211d69959950b
akira2542 <akiraakamura@gmail.com>
2022-05-27 15:20:01 +0700

    add example

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -3,5 +3,43 @@
 </template>
 
 <script setup lang="ts">
+// import { useEapi } from '@/providers';
+// import { computed } from '@vue/reactivity';
+// import { onMounted, reactive, ref, watch } from '@vue/runtime-dom';
+// import { useRoute, useRouter } from 'vue-router';
+// import { number } from 'yup';
+
+// const count = ref(0);
+// const dataObj = reactive({ 
+//   a: 1,
+//   b: 2,
+// })
+
+// const a = computed(() => count.value + 2);
+
+// watch(dataObj,(newVal, oldVal) => {
+//   console.log('a has changed!')
+// })
+
+// const eapi = useEapi();
+// const fetchData = async () => {
+//   await eapi.healthCheck();
+// }
+
+// onMounted(() => {
+
+// })
+
+// const route = useRoute()
+// const token = route.params.token;
+
+// const router = useRouter();
+// const go = () => {
+//   router.push('/a/b')
+// }
+
+
+
+
 import LayoutContainer from "@/components/Layout/LayoutContainer.vue";
 </script>
